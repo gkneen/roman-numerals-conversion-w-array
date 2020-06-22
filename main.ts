d7efa15e34659d2f4503d877883be8fe8c5d1ea0
gkneen <67211173+gkneen@users.noreply.github.com>
2020-06-21 20:55:30 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,12 @@
 let offset = 0
 let digit = 0
 let arabicNumber = 107
-let ANasText = convertToText(arabicNumber)
+let anAsText = convertToText(arabicNumber)
 let romanNumber = ""
 let romanDigit = ""
 let romanNumerals = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM", "M", "MM", "MMM"]
-for (let index = 0; index <= ANasText.length - 1; index++) {
-    digit = parseFloat(ANasText.charAt(ANasText.length - index - 1))
+for (let index = 0; index <= anAsText.length - 1; index++) {
+    digit = parseFloat(anAsText.charAt(anAsText.length - index - 1))
     offset = index * 9
     if (digit != 0) {
         romanDigit = romanNumerals[offset + (digit - 1)]
